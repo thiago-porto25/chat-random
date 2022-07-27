bedfc27aba7799d265234ecdce4cc6291e7be777
Thiago Porto <thiago.porto@d3.do>
2022-07-27 20:16:03 -0300

    test(cypress): add cucumber processor to cypress
    With this processor we can write tests in the Gherkin language

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,55 @@
 import { defineConfig } from "cypress"
 
+import webpack from "@cypress/webpack-preprocessor"
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor"
+
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+  await addCucumberPreprocessorPlugin(on, config)
+
+  on(
+    "file:preprocessor",
+    webpack({
+      webpackOptions: {
+        resolve: {
+          extensions: [".ts", ".js"],
+        },
+        module: {
+          rules: [
+            {
+              test: /\.ts$/,
+              exclude: [/node_modules/],
+              use: [
+                {
+                  loader: "ts-loader",
+                },
+              ],
+            },
+            {
+              test: /\.feature$/,
+              use: [
+                {
+                  loader: "@badeball/cypress-cucumber-preprocessor/webpack",
+                  options: config,
+                },
+              ],
+            },
+          ],
+        },
+      },
+    })
+  )
+
+  return config
+}
+
 export default defineConfig({
   e2e: {
     defaultCommandTimeout: 10000,
     baseUrl: "http://localhost:3000",
+    specPattern: "**/*.feature",
+    setupNodeEvents,
   },
 })
