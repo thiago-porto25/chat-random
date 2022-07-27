582ecfabfd7454f5c762ef9e800a02efe09d15b5
Thiago Porto <thiago.porto@d3.do>
2022-07-27 19:47:54 -0300

    chore(firebase): config firebase
    create config file for firebase and create a function to create users for testing

---
+++ src/firebase/config.ts
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app"
+import { getAuth, connectAuthEmulator } from "firebase/auth"
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore"
+
+import { createTestUser } from "./createTestUser"
+
+const config = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+}
+
+const app = initializeApp(config)
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+if (process.env.NODE_ENV === "development") {
+  connectFirestoreEmulator(db, "localhost", 9098)
+  connectAuthEmulator(auth, "http://localhost:9099")
+  const result = createTestUser(auth)
+
+  if (!result) throw new Error("Failed to create test user")
+}
+
+export { db, auth }
