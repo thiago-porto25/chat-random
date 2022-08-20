55be2b096d471e02a3c31c89f45ca6c9509ce4bb
Thiago Porto <thiago.porto@d3.do>
2022-08-20 11:33:29 -0300

    chore: remove Spinner component

--- src/shared/components/Spinner/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-import { testId } from "@src/test-utils"
-
-import { Loader } from "./styles"
-
-export const Spinner: React.FC = () => {
-  return <Loader data-testid={testId.spinner}>Loading...</Loader>
-}
